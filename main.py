b4134710d7286652a6a0c97ddcbb6c3ca8ecc135
pawelzakieta <pawelzakieta97@gmail.com>
2023-11-02 01:16:09 +0100

    circle demo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from models.circle import Circle
 from models.multi_mesh import MultiMesh
 from models.sphere import Sphere
 from visualisation.visobject import VisObject
@@ -8,11 +9,12 @@ from visualisation.meshViewer import MeshViewWindow
 
 if __name__ == "__main__":
     sphere = Sphere(radius=0.05, vert_count=14)
+    c = Circle()
     win = MeshViewWindow().init_default()
-    for i in range(50):
+    for i in range(5):
         # sphere = Sphere(radius=0.1, vert_count=500)
         spheres = MultiMesh(sphere, count=4000)
         positions = np.random.random((4000, 3)) * 15 - 8
         spheres.set_positions(positions)
-        win.add_object(VisObject(spheres))
+        win.add_object(VisObject(c))
     win.run()
--- models/circle.py
+++ models/circle.py
@@ -4,16 +4,22 @@ from models.mesh import Mesh
 from models.cube import get_unit_cube_vertices
 
 
-class Sphere(Mesh):
+class Circle(Mesh):
     def __init__(self, radius: float = 1.0, vert_count=30):
-        angles = np.arange(0, np.pi * 2, vert_count, endpoints=False)
+        angles = np.linspace(0, np.pi * 2, vert_count, endpoint=False)
         x = np.cos(angles) * radius
         y = np.sin(angles) * radius
-        vertices = np.zeros((vert_count, 3))
-        vertices[:, 0] = x
-        vertices[:, 1] = y
+        vertices = np.zeros((vert_count + 1, 3))
+        vertices[1:, 0] = x
+        vertices[1:, 1] = y
         normals = np.zeros((vert_count, 3))
         normals[:, 2] = 1
         triangle_indices = np.zeros((vert_count, 3))
+        triangle_indices[:, 1] = np.arange(vert_count) + 1
+        triangle_indices[:-1, 2] = np.arange(1, vert_count) + 1
+        triangle_indices[-1, 2] = 1
+        super().__init__(vertices, triangle_indices, normals=normals)
 
 
+if __name__ == '__main__':
+    s = Circle(vert_count=4)
