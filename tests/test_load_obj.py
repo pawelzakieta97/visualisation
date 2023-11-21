69b4c56eeaa7a70e8e87f2638fc7a3e947f5bd53
pawelzak <pawel.zakieta@nask.pl>
2023-11-21 12:35:34 +0100

    obj load

---
+++ tests/test_load_obj.py
@@ -0,0 +1,12 @@
+from models.mesh import Mesh
+from models.parse_obj import parse_obj
+from transformations import get_scale_matrix
+from visualisation.meshViewer import MeshViewWindow
+
+if __name__ == "__main__":
+    vertices, triangles = parse_obj('../obj/bunny.obj')
+    bunny = Mesh(vertices=vertices, triangle_indices=triangles)
+    bunny.transform(get_scale_matrix(10,10,10))
+    win = MeshViewWindow(add_floorgrid=True, orthographic=True, target_fps=10)
+    win.add_object(bunny)
+    win.run()
