fab8cde11014026c2566ef5095995ea976a1fdc0
pawelzak <pawel.zakieta@nask.pl>
2023-11-27 14:21:22 +0100

    icosahedron based sphere

--- models/primitives/sphere.py
+++ models/primitives/sphere.py
@@ -5,15 +5,55 @@ from models.primitives.cone import Cone
 from models.primitives.cube import get_unit_cube_vertices
 
 
+def get_icosahedron():
+    angles = np.linspace(0, np.pi * 2, 5, endpoint=False)
+    n = 5
+    total_angle = (n - 2) * np.pi
+    angle = total_angle / n / 2
+    R = 1 / np.cos(angle) / 2
+    r = R * np.sin(angle)
+    x1 = np.cos(angles) * R
+    y1 = np.sin(angles) * R
+    h1 = (1 - R ** 2) ** 0.5
+    th = (3 ** 0.5)/2
+    h2 = (th ** 2 - (R - r) ** 2) ** 0.5
+    angles = np.linspace(0, np.pi * 2, 5, endpoint=False) + np.pi / 5
+    x2 = np.cos(angles) * R
+    y2 = np.sin(angles) * R
+    vertices = np.zeros((12, 3))
+    vertices[0, 2] = -h1 - h2/2
+    vertices[1:6, 0] = x1
+    vertices[1:6, 1] = y1
+    vertices[1:6, 2] = - h2/2
+    vertices[6:11, 0] = x2
+    vertices[6:11, 1] = y2
+    vertices[6:11, 2] = h2/2
+    vertices[11, 2] = h1 + h2/2
+    triangles = np.zeros((20, 3)).astype(int)
+    triangles[:5, 0] = 0
+    triangles[:5, 1] = [1, 2, 3, 4, 5]
+    triangles[:5, 2] = [2, 3, 4, 5, 1]
+
+    triangles[5:10, 1] = [1, 2, 3, 4, 5]
+    triangles[5:10, 0] = [2, 3, 4, 5, 1]
+    triangles[5:10, 2] = [6, 7, 8, 9, 10]
+
+    triangles[10:15, 0] = [6, 7, 8, 9, 10]
+    triangles[10:15, 1] = [7, 8, 9, 10, 6]
+    triangles[10:15, 2] = [2, 3, 4, 5, 1]
+
+    triangles[15:, 1] = [6, 7, 8, 9, 10]
+    triangles[15:, 0] = [7, 8, 9, 10, 6]
+    triangles[15:, 2] = 11
+    return vertices, triangles
+
+
 class Sphere(Mesh):
-    def __init__(self, radius: float = 1.0, smoothness=2,
+    def __init__(self, radius: float = 1.0, smoothness=1,
                  smooth=True):
-        r = 1/3**0.5
-        h = (1 - r**2)**0.5
-        tetrahedron = Cone(radius=r, height=h, segments=3, single_top=True, center_point=False, separate_base=False)
-        vertices = tetrahedron.vertices - np.array([0, 0.3, 0])
-        triangle_indices = tetrahedron.triangle_indices
-        super().__init__(vertices, triangle_indices)
+        vertices, triangles = get_icosahedron()
+        super().__init__(vertices, triangles)
+        self.flip_normals()
         for i in range(smoothness):
             self.subdivide2()
 
@@ -23,4 +63,3 @@ class Sphere(Mesh):
             self.flatten()
         self.normals = self.get_normals()
         pass
-
